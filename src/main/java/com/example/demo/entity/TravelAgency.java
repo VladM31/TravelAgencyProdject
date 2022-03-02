7f33dc2cdb5e4706e4f67ab8ac3c7d5c698fac78
Владислав Мормуль <vladyslav.mormul@nure.ua>
2022-03-03 00:34:24 +0200

    add travel agency

---
+++ src/main/java/com/example/demo/entity/TravelAgency.java
@@ -0,0 +1,91 @@
+package com.example.demo.entity;
+
+public class TravelAgency {
+    private Long travelId;
+    private int rating;
+    private int kved;
+    private Long egrpoy;
+    private Long rnekpn;
+    private String address;
+    private String allNameDirector;
+    private String describeAgency;
+
+    public Long getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(Long travelId) {
+        this.travelId = travelId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getKved() {
+        return kved;
+    }
+
+    public void setKved(int kved) {
+        this.kved = kved;
+    }
+
+    public Long getEgrpoy() {
+        return egrpoy;
+    }
+
+    public void setEgrpoy(Long egrpoy) {
+        this.egrpoy = egrpoy;
+    }
+
+    public Long getRnekpn() {
+        return rnekpn;
+    }
+
+    public void setRnekpn(Long rnekpn) {
+        this.rnekpn = rnekpn;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAllNameDirector() {
+        return allNameDirector;
+    }
+
+    public void setAllNameDirector(String allNameDirector) {
+        this.allNameDirector = allNameDirector;
+    }
+
+    public String getDescribeAgency() {
+        return describeAgency;
+    }
+
+    public void setDescribeAgency(String describeAgency) {
+        this.describeAgency = describeAgency;
+    }
+
+    public TravelAgency(Long travelId, int rating, int kved, Long egrpoy, Long rnekpn, String address, String allNameDirector, String describeAgency) {
+        this.travelId = travelId;
+        this.rating = rating;
+        this.kved = kved;
+        this.egrpoy = egrpoy;
+        this.rnekpn = rnekpn;
+        this.address = address;
+        this.allNameDirector = allNameDirector;
+        this.describeAgency = describeAgency;
+    }
+    public TravelAgency(){
+
+    }
+
+}
