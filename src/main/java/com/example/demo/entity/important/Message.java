5a0c090f808b3cda8f1f5949a6564768ccf95f03
Владислав Мормуль <vladyslav.mormul@nure.ua>
2022-04-25 15:29:06 +0300

    change Message class

--- src/main/java/com/example/demo/entity/important/Message.java
+++ src/main/java/com/example/demo/entity/important/Message.java
@@ -8,8 +8,6 @@ public class Message {
     private String name;
     private String describeMess;
     private LocalDateTime dateSend;
-    private Long fromWhomId;
-    private Set<Long> toWhomIds;
 
     public Long getId() {
         return id;
@@ -43,29 +41,12 @@ public class Message {
         this.dateSend = dateSend;
     }
 
-    public Long getFromWhomId() {
-        return fromWhomId;
-    }
-
-    public void setFromWhomId(Long fromWhomId) {
-        this.fromWhomId = fromWhomId;
-    }
-
-    public Set<Long> getToWhomIds() {
-        return toWhomIds;
-    }
-
-    public void setToWhomIds(Set<Long> toWhomIds) {
-        this.toWhomIds = toWhomIds;
-    }
-
-    public Message(Long id, String name, String describeMess, LocalDateTime dateSend, Long fromWhomId, Set<Long> toWhomIds) {
+    public Message(Long id, String name, String describeMess, LocalDateTime dateSend) {
         this.id = id;
         this.name = name;
         this.describeMess = describeMess;
         this.dateSend = dateSend;
-        this.fromWhomId = fromWhomId;
-        this.toWhomIds = toWhomIds;
+
     }
 
     public Message() {
@@ -79,17 +60,15 @@ public class Message {
         Message message = (Message) o;
 
         if (!id.equals(message.id)) return false;
-        if (!dateSend.equals(message.dateSend)) return false;
-        if (!fromWhomId.equals(message.fromWhomId)) return false;
-        return toWhomIds.equals(message.toWhomIds);
+
+
+        return dateSend.equals(message.dateSend);
     }
 
     @Override
     public int hashCode() {
         int result = id.hashCode();
         result = 31 * result + dateSend.hashCode();
-        result = 31 * result + fromWhomId.hashCode();
-        result = 31 * result + toWhomIds.hashCode();
         return result;
     }
 }
---
+++ src/main/java/com/example/demo/entity/subordinate/MessageConection.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity.subordinate;
+
+import com.example.demo.entity.important.Message;
+
+import java.util.Set;
+
+public class MessageConection {
+    private Message message;
+    private Long fromWhomId;
+    private Set<Long> toWhomIds;
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public Long getFromWhomId() {
+        return fromWhomId;
+    }
+
+    public void setFromWhomId(Long fromWhomId) {
+        this.fromWhomId = fromWhomId;
+    }
+
+    public Set<Long> getToWhomIds() {
+        return toWhomIds;
+    }
+
+    public void setToWhomIds(Set<Long> toWhomIds) {
+        this.toWhomIds = toWhomIds;
+    }
+
+    public MessageConection(Message message, Long fromWhomId, Set<Long> toWhomIds) {
+        this.message = message;
+        this.fromWhomId = fromWhomId;
+        this.toWhomIds = toWhomIds;
+    }
+
+    public MessageConection() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MessageConection that = (MessageConection) o;
+
+        if (!message.equals(that.message)) return false;
+        if (!fromWhomId.equals(that.fromWhomId)) return false;
+        return toWhomIds.equals(that.toWhomIds);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = message.hashCode();
+        result = 31 * result + fromWhomId.hashCode();
+        result = 31 * result + toWhomIds.hashCode();
+        return result;
+    }
+}
