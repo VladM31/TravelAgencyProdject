d67f1d13cb9121294692ebd89714c67cd3eda3f8
Владислав Мормуль <vladyslav.mormul@nure.ua>
2022-06-04 00:20:55 +0300

    DAOSecurityMySQL update work with dependency injection

--- src/main/java/nure/knt/database/dao/mysql/registration/DAOSecurityMySQL.java
+++ src/main/java/nure/knt/database/dao/mysql/registration/DAOSecurityMySQL.java
@@ -15,70 +15,32 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Component("DA0_SECURITY_MYSQL")
 public class DAOSecurityMySQL implements IDAOSecurity {
 
-    private IDAOCourierSQL<Courier> daoCourier;
-    private IDAOTravelAgencySQL<TravelAgency> daoTravelAgency;
-    private IDAOCustomerSQL<Customer> daoCustomer;
-    private IDAOUserSQL<User> daoUser;
+    private List<IDAOUserSQL> daoUsers;
 
     @Autowired
-    @Qualifier("DAO_COURIER_MYSQL")
-    public void setDaoCourier(IDAOCourierSQL<Courier> daoCourier) {
-        this.daoCourier = daoCourier;
+    public void setDaoUsers(List<IDAOUserSQL> daoUsers) {
+        this.daoUsers = daoUsers;
     }
-
-    @Autowired
-    @Qualifier("DA0_TRAVEL_AGENCY_MYSQL")
-    public void setDaoTravelAgency(IDAOTravelAgencySQL<TravelAgency> daoTravelAgency) {
-        this.daoTravelAgency = daoTravelAgency;
-    }
-
-    @Autowired
-    @Qualifier("DAO_CUSTOMER_MYSQL")
-    public void setDaoCustomer(IDAOCustomerSQL<Customer> daoCustomer) {
-        this.daoCustomer = daoCustomer;
-    }
-
-    @Autowired
-    @Qualifier("DAO_USER_MYSQL")
-    public void setDaoUser(IDAOUserSQL<User> daoUser) {
-        this.daoUser = daoUser;
-    }
-
     private static final User USER_NOT_FOUND = null;
 
     @Nullable
     @Override
     public User getUserByUsernameDependingOnRole(String username) {
-        CheckToNull checkToNull = new CheckToNull(
-                daoCourier::findByUsername,
-                daoTravelAgency::findByUsername,
-                daoCustomer::findByUsername,
-                daoUser::findByUsername);
-
-        return checkToNull.find(username);
-    }
-
-    private static class CheckToNull{
-        private Function<String,User>[] functions;
-
-        CheckToNull(@NonNull Function<String,User> ...functions){
-            this.functions = functions;
-        }
-
-        public User find(String username){
-            User user = USER_NOT_FOUND;
-            for (Function<String,User> function : functions){
-                user = function.apply(username);
-                if (user != USER_NOT_FOUND){
-                    return user;
-                }
+        User user = USER_NOT_FOUND;
+        for (IDAOUserSQL dao:daoUsers) {
+            user = dao.findByUsername(username);
+            if(user != USER_NOT_FOUND){
+                return user;
             }
-            return USER_NOT_FOUND;
         }
+        return USER_NOT_FOUND;
     }
+
+
 }
