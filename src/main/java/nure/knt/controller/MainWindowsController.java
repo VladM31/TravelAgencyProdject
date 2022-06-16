95a48e01392179c4a60c01cbbc724f4d7a148f48
Владислав Мормуль <vladyslav.mormul@nure.ua>
2022-06-16 23:03:30 +0300

    added page for find all tour ads

--- src/main/java/nure/knt/controller/MainWindowsController.java
+++ src/main/java/nure/knt/controller/MainWindowsController.java
@@ -1,10 +1,11 @@
 package nure.knt.controller;
 
 import nure.knt.database.idao.entity.IDAOTravelAgencySQL;
-import nure.knt.entity.enums.Role;
 import nure.knt.entity.important.TravelAgency;
 import nure.knt.entity.important.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +15,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
-public class MainWindows {
+@PropertySource("classpath:MainPages.properties")
+@PropertySource("classpath:customer.properties")
+public class MainWindowsController {
+
+    private final String PAGE_TOUR_ADS;
+    private final String REDIRECT_ON_CHECKOUT_CUSTOMER;
+
+    @Autowired
+    public MainWindowsController(@Value("${main.pages.find.all.name.page}") String PAGE_TOUR_ADS,
+                                 @Value("${main.pages.find.all.url}") String REDIRECT_ON_CHECKOUT_CUSTOMER) {
+        this.PAGE_TOUR_ADS = PAGE_TOUR_ADS;
+        this.REDIRECT_ON_CHECKOUT_CUSTOMER = REDIRECT_ON_CHECKOUT_CUSTOMER;
+    }
+
     // ************* Головна сторінка *****************
     private static final String DIRECTORY = "main_pages/";
 
@@ -47,6 +61,10 @@ public class MainWindows {
         return PAGE_HELLO;
     }
 
+    @RequestMapping(value = "${main.pages.find.all.url}", method = {RequestMethod.GET})
+    public String showPageForFindAllTourAd(){
+        return PAGE_TOUR_ADS;
+    }
 
 }
 
@@ -65,43 +83,45 @@ class HandlerMainWindows{
 
         return nodes;
     }
-}
 
-class NodeForMainPage{
-    private TravelAgency travelAgency;
-    private String style;
-    private List<String> stars;
+    public static class NodeForMainPage{
+        private TravelAgency travelAgency;
+        private String style;
+        private List<String> stars;
 
-    public NodeForMainPage(TravelAgency travelAgency, String color, List<String> stars) {
-        this.travelAgency = travelAgency;
-        this.style = color;
-        this.stars = stars;
+        public NodeForMainPage(TravelAgency travelAgency, String color, List<String> stars) {
+            this.travelAgency = travelAgency;
+            this.style = color;
+            this.stars = stars;
+        }
+
+        public NodeForMainPage() {
+        }
+
+        public TravelAgency getTravelAgency() {
+            return travelAgency;
+        }
+
+        public void setTravelAgencies(TravelAgency travelAgency) {
+            this.travelAgency = travelAgency;
+        }
+
+        public String getStyle() {
+            return style;
+        }
+
+        public void setStyle(String style) {
+            this.style = style;
+        }
+
+        public List<String> getStars() {
+            return stars;
+        }
+
+        public void setStars(List<String> stars) {
+            this.stars = stars;
+        }
+
     }
 
-    public NodeForMainPage() {
-    }
-
-    public TravelAgency getTravelAgency() {
-        return travelAgency;
-    }
-
-    public void setTravelAgencies(TravelAgency travelAgency) {
-        this.travelAgency = travelAgency;
-    }
-
-    public String getStyle() {
-        return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
-    public List<String> getStars() {
-        return stars;
-    }
-
-    public void setStars(List<String> stars) {
-        this.stars = stars;
-    }
 }
