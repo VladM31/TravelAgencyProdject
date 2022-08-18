78f5bfb0c1b84e0effd297f80ffd07b802e35997
Владислав Мормуль <vladyslav.mormul@nure.ua>
2022-08-18 19:07:48 +0300

    update DAOSecurityMySQL

--- src/main/java/nure/knt/database/dao/mysql/registration/DAOSecurityMySQL.java
+++ src/main/java/nure/knt/database/dao/mysql/registration/DAOSecurityMySQL.java
@@ -1,31 +1,24 @@
 package nure.knt.database.dao.mysql.registration;
 
 import nure.knt.database.idao.IDAOSecurity;
-import nure.knt.database.idao.entity.*;
-import nure.knt.database.idao.terms.ITermInformation;
 import nure.knt.database.idao.terms.users.ITermUser;
+import nure.knt.database.service.implement.users.IServiceUser;
 import nure.knt.entity.enums.TypeState;
-import nure.knt.entity.important.Courier;
-import nure.knt.entity.important.Customer;
-import nure.knt.entity.important.TravelAgency;
 import nure.knt.entity.important.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Component("DA0_MySQL_Security")
 public class DAOSecurityMySQL implements IDAOSecurity {
 
-    private List<IDAOUserWithTerms<? extends User,? extends ITermUser>> daoUserList;
+    private List<IServiceUser<? extends User,? extends ITermUser>> serviceUsers;
     @Autowired
-    public void setParameters( List<IDAOUserWithTerms<? extends User,? extends ITermUser>>  list){
-        this.daoUserList = list;
+    public void setParameters( List<IServiceUser<? extends User,? extends ITermUser>>  list){
+        this.serviceUsers = list;
     }
 
     private static final User USER_NOT_FOUND = null;
@@ -34,9 +27,9 @@ public class DAOSecurityMySQL implements IDAOSecurity {
     @Override
     public User getUserByUsernameDependingOnRole(String username) {
         Optional<? extends User> userOptional = null;
-        for (IDAOUserWithTerms<? extends User,? extends ITermUser> dao:daoUserList) {
-            userOptional = dao
-                    .findOneBy(dao.term().usernameIs(username)
+        for (IServiceUser<? extends User,? extends ITermUser> service: serviceUsers) {
+            userOptional = service
+                    .findOneBy(service.term().usernameIs(username)
                     .typeStateIn(TypeState.REGISTERED).end());
             if(userOptional.isPresent()){
                 return userOptional.get();
